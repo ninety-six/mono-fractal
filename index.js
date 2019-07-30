b6a1a5e625838b1c146b514f979efd6efa5de19a
Ben Hall <ben@accentdesign.co.uk>
2019-07-30 11:10:43 +0100

    Initial files added

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+'use strict';
+
+const mandelbrot = require('@frctl/mandelbrot');
+
+/*
+ * Configure the theme
+ */
+const subTheme = mandelbrot({
+    nav: ['docs', 'components'],
+    skin: 'white',
+    styles: ['default', '/mono-theme/webui.css'],
+    panels: ['view', 'context', 'html']
+});
+
+/*
+ * Specify a template directory to override any view templates
+ */
+subTheme.addLoadPath(__dirname + '/views');
+
+/*
+ * Specify the static assets directory that contains the custom stylesheet.
+ */
+subTheme.addStatic(__dirname + '/assets', '/mono-theme');
+
+/*
+ * Export the customised theme instance so it can be used in Fractal projects
+ */
+module.exports = subTheme;
