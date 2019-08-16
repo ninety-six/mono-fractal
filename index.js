3d2db269902268405983485647b753a2f062e647
Ben Hall <ben@accentdesign.co.uk>
2019-08-16 10:23:06 +0100

    Refactored to handle reconfig of subtheme

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
-// index.js
 'use strict';
 
 const mandelbrot = require('@frctl/mandelbrot');
+const _ = require('lodash');
 
 /*
- * Configure the theme
+ * Required config overrides incoming options
+ * Note: styles array is merged
  */
-const subTheme = mandelbrot({
-    nav: ['docs', 'components'],
-    skin: 'white',
-    styles: ['default', '/mono-theme/webui.min.css', '/webui-overrides/overrides.css'],
-    panels: ['view', 'context', 'html'],
-    favicon: '/mono-theme/favicon.ico'
-});
-
-/*
- * Specify a template directory to override any view templates
- */
-subTheme.addLoadPath(__dirname + '/webui-overrides');
-
-/*
- * Specify the static assets directory that contains the custom stylesheet.
- */
-subTheme.addStatic(__dirname + '/assets', '/mono-theme');
+const requiredConfig = {
+    styles: [
+        'default',
+        '/mono-theme/webui.min.css'
+    ],
+    scripts: ['default']
+};
 
 /*
  * Export the customised theme instance so it can be used in Fractal projects
  */
-module.exports = subTheme;
+module.exports = function(options) {
+    /*
+     * Configure the theme
+     */
+    const styles = _.union(requiredConfig.styles, options.styles || []);
+    const scripts = _.union(requiredConfig.scripts, options.scripts || []);
+
+    const config = _.defaultsDeep({
+        skin: 'white',
+        styles: styles,
+        scripts: scripts
+    }, _.clone(options || {}));
+
+    config.panels = config.panels || ['view', 'context', 'html'];
+    config.nav = config.nav || ['docs', 'components'];
+    config.favicon = config.favicon || '/mono-theme/favicon.ico';
+
+    const subTheme = mandelbrot(config);
+
+    /*
+     * Specify a template directory to override any view templates
+     */
+    subTheme.addLoadPath(__dirname + '/webui-overrides');
+
+    /*
+     * Specify the static assets directory that contains the custom stylesheet.
+     */
+    subTheme.addStatic(__dirname + '/assets', '/mono-theme');
+
+    /*
+     * Provide for further configuration
+     */
+    return subTheme;
+};
